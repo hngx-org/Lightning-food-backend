0b5d782a88ab36f1cb2779ce148413d79411aa96
NwobiaDavid <dnwobia@gmail.com>
2023-09-20 08:11:16 +0100

    imported the index and users router from the outes folder and i added some comments to better explain the code

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
+// Import required packages
 var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// middleware
-app.use(express.json())
+// Create an Express application
+var app = express();
 
-
+// Configure middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Define and use your routers (Assuming these are properly defined)
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+// Error handler
+app.use(function(err, req, res, next) {
+    // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // Render the error page using the 'error' view (assuming EJS is used)
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error'); // This assumes you have an 'error.ejs' template in your views folder
 });
 
 module.exports = app;
