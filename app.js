759a000a1f0fe8490b4b7dce29921d506290926c
tope101 <temitope@kobo360.com>
2023-09-20 09:32:08 +0100

    remove controller code and fix conflicts

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const URI = process.env.MYSQL_ADDON_URI;
-const PORT = process.env.PORT || 4000;
+
+const URI = process.env.MYSQL_ADDON_URI
 
 
-const userRoutes = require('./routes/users');
-
-
-app.use('/users', userRoutes);
-
-//db connction here
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen()
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const { getUserById } = require('../controllers/userController');
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// GET get user or staff details by ID
-router.get('/:id', getUserById);
-
 module.exports = router;
-
-//I think the user routes should be named userRoutes.js instead of users.js 
