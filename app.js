b8d0651d5a67f801d285c3d640f4f9ac6b21a382
tope101 <temitope@kobo360.com>
2023-09-20 10:22:51 +0100

    creates a GET endpoint to get user details by ID

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const URI = process.env.MYSQL_ADDON_URI;
+const PORT = process.env.PORT || 4000;
 
-const URI = process.env.MYSQL_ADDON_URI
+const userRoutes = require('./routes/users');
 
+app.use('/users', userRoutes);
 
-app.listen()
+//db connction here
+
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+const { Sequelize } = require('sequelize');
+
+const User = require('../models/auth.model.js'); //the name of the user model was auth.model in Federick's code
+
+// Controller function to get user/staff details by UUID
+async function getUserById(req, res) {
+    try {
+        const userId = req.params.id; 
+        const user = await User.findOne({ where: { id: userId } });
+    
+        if (!user) {
+          return res.status(404).json({
+            success: false,
+            message: 'User not found',
+            data: null,
+          });
+        }
+    
+        res.status(200).json({
+          success: true,
+          message: 'User found',
+          data: {
+            user: user,
+          },
+        });
+      } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({
+          success: false,
+          message: 'Internal Server Error',
+          data: null,
+        });
+        }
+}
+
+module.exports = {
+getUserById,
+};
