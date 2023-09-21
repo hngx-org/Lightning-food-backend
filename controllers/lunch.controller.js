231a253221e201e6d0cb785655294b432c8b7645
DevTinaTi <tinadesk8@gmail.com>
2023-09-21 20:23:21 +0100

    add the redeem lunch controller

--- controllers/lunch.controller.js
+++ controllers/lunch.controller.js
@@ -4,9 +4,11 @@ const { Lunch } = require(join(__dirname, "..", "models", "index"));
 
 export const redeemedLunch = async (req, res) => {
     //req.user is equal to the user's id stored in the authentication middleware
+    //id is the id of the lunch being passed throught url params
+    const {id} = req.params;
 await Lunch.update(
     { redeemed: true },
-    { where: { senderId: req.user} }
+    { where: { id:id, senderId: req.user} }
   );
   return res.status(200).json({
     success: true,
