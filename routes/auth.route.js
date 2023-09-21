901bafacfabc657d0bd0ca2732ab2c1bf68a85fb
edahmitchel <edahmitchel@gmail.com>
2023-09-21 18:18:34 +0100

    added create password to auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const userLoginController = require('../controllers/userLoginController');
 const { createUser } = require('../controllers/userController');
+const auth = require('../middlewares/auth');
+const createPasswordController = require('../controllers/createPasswordController');
 
 const router = express.Router();
 
 router.post('/login', userLoginController);
 router.post('/user/signup', createUser);
+router.post('/user/create-password', auth, createPasswordController);
 
 module.exports = router;
